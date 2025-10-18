43cd1e5ec0b05080ecd013111e17977820c00ca4
shadow001-man <claudiochivindas145@gmail.com>
2025-10-18 23:30:25 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -36,16 +36,15 @@ module.exports = {
 			confirmThisThread: "Vui lòng thả cảm xúc bất kỳ vào tin nhắn này để xác nhận thay đổi prefix trong nhóm chat của bạn",
 			successGlobal: "Đã thay đổi prefix hệ thống bot thành: %1",
 			successThisThread: "Đã thay đổi prefix trong nhóm chat của bạn thành: %1",
-			myPrefix: "🦋𝘼𝙎𝙎𝘼𝙇𝘼𝙈𝙐𝘼𝙇𝘼𝙄𝙆𝙐𝙈✨"
-				+ "\n                       "
-				+ "\n 𝙃𝙚𝙡𝙡𝙤 𝙢𝙮 𝙣𝙖𝙢𝙚 𝙞𝙨 ♡𝕮𝖍𝖔𝖈𝖔𝖑𝖆𝖙𝖊 𝕼𝖚𝖊𝖊𝖓♡"
-				+ "\n 𝙃𝙚𝙧𝙚 𝙞𝙨 𝙢𝙮 ❇️ 𝙋𝙍𝙀𝙁𝙄𝙓 ❇️"
-				+ "\n                         "
-				+ "\n ~🌀 𝙎𝙔𝙎𝙏𝙀𝙈 𝙋𝙍𝙀𝙁𝙄𝙓: %1"
-				+ "\n ~♻️ 𝙋𝙍𝙀𝙁𝙄𝙓 𝙁𝙊𝙍 𝙏𝙃𝙀 𝙂𝙍𝙊𝙐𝙋: %2"
-				+ "\n                         "
-				+ "\n 💠 𝙃𝙀𝙍𝙀 𝙄𝙎 𝙈𝙔 𝙊𝙒𝙉𝙀𝙍 💠"
-				+ "\n ~𝙁𝘽✨:https://www.facebook.com/sirana252"
+			myPrefix: "╭‣ 𝐂𝐈𝐃 𝐁𝐨𝐭 🎭
+				+ "\n  ╰‣ ❀𝐒𝐇𝐀𝐃𝐎𝐖❀ " 
+				+ "\n "	                         
+				+ "\n ~🌀𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:: %1"
+				+ "\n ~🏌𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱: %2"
+				+ "\n "
+				+ "\n ╭‣ 𝐂𝐈𝐃 ⓕ
+				+ "\n ╰‣ m.me/cid.kagenou2020
+"
 		},
 		en: {
 			reset: "🙆| Your prefix has been reset to default: %1",
@@ -54,16 +53,14 @@ module.exports = {
 			confirmThisThread: "⚠️| Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "♻️| Changed prefix of system bot to: %1",
 			successThisThread: "♻️| Changed prefix in your box chat to: %1",
-			myPrefix: "🦋𝘼𝙎𝙎𝘼𝙇𝘼𝙈𝙐𝘼𝙇𝘼𝙄𝙆𝙐𝙈✨"
-				+ "\n                       "
-				+ "\n 𝙃𝙚𝙡𝙡𝙤 𝙢𝙮 𝙣𝙖𝙢𝙚 𝙞𝙨 ♡𝕮𝖍𝖔𝖈𝖔𝖑𝖆𝖙𝖊 𝕼𝖚𝖊𝖊𝖓♡"
-				+ "\n 𝙃𝙚𝙧𝙚 𝙞𝙨 𝙢𝙮 ❇️ 𝙋𝙍𝙀𝙁𝙄𝙓 ❇️"
-				+ "\n                         "
-				+ "\n ~🌀 𝙎𝙔𝙎𝙏𝙀𝙈 𝙋𝙍𝙀𝙁𝙄𝙓: %1"
-				+ "\n ~♻️ 𝙋𝙍𝙀𝙁𝙄𝙓 𝙁𝙊𝙍 𝙏𝙃𝙀 𝙂𝙍𝙊𝙐𝙋: %2"
-				+ "\n                         "
-				+ "\n 💠 𝙃𝙀𝙍𝙀 𝙄𝙎 𝙈𝙔 𝙊𝙒𝙉𝙀𝙍 💠"
-				+ "\n ~𝙁𝘽✨:https://www.facebook.com/sirana252"
+			myPrefix: "╭‣ 𝐂𝐈𝐃 𝐁𝐨𝐭 🎭
+				+ "\n  ╰‣ ❀𝐒𝐇𝐀𝐃𝐎𝐖❀ " 
+				+ "\n "	                         
+				+ "\n ~🌀𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:: %1"
+				+ "\n ~🏌𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱: %2"
+				+ "\n "
+				+ "\n ╭‣ 𝐂𝐈𝐃 ⓕ
+				+ "\n ╰‣ m.me/cid.kagenou2020 "
 		}
 	},
 
